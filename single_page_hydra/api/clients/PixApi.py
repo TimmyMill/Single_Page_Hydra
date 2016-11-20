d83ef0fdcbb10a668f291f4c0e6c105b139a6070
ShivonQ <realitykeeper@gmail.com>
2016-11-20 16:14:21 -0600

    Tweaked Colors Further

--- single_page_hydra/api/clients/PixApi.py
+++ single_page_hydra/api/clients/PixApi.py
@@ -1,23 +1,21 @@
 import requests
 
-from single_page_hydra.api import key
+from single_page_hydra.api import keys as KEY
 
 
 class pixapi:
     def __init__(self):
-        self.key = '?key='+(key.key)
+        self.key = '?key=' + (KEY.PIX_KEY)
         self.base_url = 'https://pixabay.com/api/'
-        self.q='&q='
-        self.image_type='&image_type=all'
-        self.safesearch='&safesearch=true'
+        self.q = '&q='
+        self.image_type = '&image_type=all'
+        self.safesearch = '&safesearch=true'
 
     def search_image(self, search_param):
-        res = requests.get(self.base_url+self.key+self.q+search_param+self.image_type+self.safesearch)
+        res = requests.get(self.base_url + self.key + self.q + search_param + self.image_type + self.safesearch)
         return res.json()
 
-
-
     def search_video(self, search_param):
-        new_base_url=self.base_url+'/videos/'
+        new_base_url = self.base_url + '/videos/'
         res = requests.get(new_base_url + self.key + self.q + search_param + self.image_type + self.safesearch)
-        return res.json()#['hits'][0]['videos']['small']['url']
+        return res.json()  # ['hits'][0]['videos']['small']['url']
