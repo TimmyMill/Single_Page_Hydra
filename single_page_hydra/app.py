6e7779e75864203c3c8e2bd8da18c6aef3507ce5
Mason Elmore <masonelmore@users.noreply.github.com>
2016-11-28 13:33:40 -0600

    Fix infinite load (#9)
    * Remove missing import
    
    * Fix search getting stuck on loading page

--- single_page_hydra/app.py
+++ single_page_hydra/app.py
@@ -5,7 +5,6 @@ from flask import (
 )
 
 from single_page_hydra.api.manager import ApiManager
-import test_resp as tr
 
 app = Flask(__name__)
 api_manager = ApiManager()
@@ -24,7 +23,6 @@ def search():
 
 @app.route('/search_results', methods=['POST'])
 def search_results():
-    # TRIED to use a spoofed object to test without calls... didnt work
     search_query = request.form.get('query')
     results = api_manager.search(search_query)
     return render_template('search_results.html', results=results, search=search_query)
