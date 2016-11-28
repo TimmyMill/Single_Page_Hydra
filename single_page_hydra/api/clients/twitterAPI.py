70d88aaf05ad2b996a35f868398cfe6a0aa2fafd
timmymill <timmymill87@gmail.com>
2016-11-28 13:08:07 -0600

    Added all 4 twitter api keys to the "keys" file. Cleaned up code in "twitterAPI"

--- single_page_hydra/api/clients/twitterAPI.py
+++ single_page_hydra/api/clients/twitterAPI.py
@@ -8,40 +8,34 @@ from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 from single_page_hydra.api.keys import *
 
 
-class TwitterAPI():
+class TwitterAPI:
 
     def __init__(self):
-        self.aouth = OAuth(Twitter_Access_Token,Twitter_Access_TokenSecret,Twitter_Consumer_Key,Twitter_Consumer_Secret)
+        self.oauth = OAuth(
+            TWITTER_ACCESS_TOKEN,
+            TWITTER_ACCESS_SECRET,
+            TWITTER_CONSUMER_KEY,
+            TWITTER_CONSUMER_SECRET
+        )
 
-
-
-        self.twitter_stream = TwitterStream(auth=self.aouth, domain='userstream.twitter.com')
-
-
-
+        self.twitter_stream = TwitterStream(auth=self.oauth, domain='userstream.twitter.com')
 
     def search(self, query):
 
-
         iterator = self.twitter_stream.statuses.filter(track=query, language="en")
-
 
         tweet_count = 10
         list_tweets = []
         for tweet in iterator:
             tweet_count -= 1
 
-
             json_tweet = json.dumps(tweet, indent=4)   # Parse the request result in json and Save in a variable
-
 
             tweets = json.loads(json_tweet)
             if 'text' in tweets:
                 tweet_text = tweets['text']
                 tweet_user = tweets['user']['name']
                 list_tweets.append({'Text': tweet_text, 'User': tweet_user})
-
-
 
             if tweet_count <= 0:
 
